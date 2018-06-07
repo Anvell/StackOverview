4a35eb736e7feb1bad08d669afdfd13771aa00ab
Denis Trotsenko <denis.tnko@gmail.com>
2018-06-07 15:49:15 +0200

    Initial commit

---
+++ app/src/main/java/io/github/anvell/stackoverview/view/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.github.anvell.stackoverview.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.anvell.stackoverview.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
---
+++ app/src/test/java/io/github/anvell/stackoverview/ExampleUnitTest.kt
@@ -0,0 +1,9 @@
+package io.github.anvell.stackoverview
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+
+}
