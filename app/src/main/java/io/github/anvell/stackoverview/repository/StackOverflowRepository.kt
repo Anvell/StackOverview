a2056129af6953da56dfd43c35033a0a6fc15feb
Denis Trotsenko <denis.tnko@gmail.com>
2018-06-09 17:36:38 +0200

    Simple repository added

--- app/src/main/java/io/github/anvell/stackoverview/repository/StackOverflowRepository.kt
+++ app/src/main/java/io/github/anvell/stackoverview/repository/StackOverflowRepository.kt
@@ -1,12 +1,38 @@
 package io.github.anvell.stackoverview.repository
 
+import android.arch.lifecycle.MutableLiveData
+import android.util.Log
 import io.github.anvell.stackoverview.model.QuestionsResponse
 import io.github.anvell.stackoverview.network.ApiClient
 import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
-class StackOverflowRepository() {
+class StackOverflowRepository {
 
-    fun searchQuestions(query: String) : Observable<QuestionsResponse>
-        = ApiClient.client.searchQuestions(query)
+    companion object {
+        private const val REQUEST_DELAY = 3000L
+    }
 
+    val connectionAvailable = MutableLiveData<Boolean>()
+
+    init {
+        connectionAvailable.value = true
+    }
+
+    private fun setConnectionStatus(available: Boolean) {
+        connectionAvailable.value?.let {
+            if(available != it) connectionAvailable.value = available
+        }
+    }
+
+    fun searchQuestions(query: String): Observable<QuestionsResponse> {
+        return ApiClient.client.searchQuestions(query)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnError { setConnectionStatus(false) }
+                .doOnNext { setConnectionStatus(true) }
+                .retryWhen { it.delay(REQUEST_DELAY, TimeUnit.MILLISECONDS) }
+    }
 }
--- app/src/main/java/io/github/anvell/stackoverview/viewmodel/MainViewModel.kt
+++ app/src/main/java/io/github/anvell/stackoverview/viewmodel/MainViewModel.kt
@@ -2,14 +2,37 @@ package io.github.anvell.stackoverview.viewmodel
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import io.github.anvell.stackoverview.model.Question
+import io.github.anvell.stackoverview.model.QuestionsResponse
+import io.github.anvell.stackoverview.repository.StackOverflowRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 
 class MainViewModel : ViewModel() {
 
-    var selectedQuestion = MutableLiveData<Question>()
-    var questions = MutableLiveData<MutableList<Question>>()
+    val selectedQuestion = MutableLiveData<Question>()
+    val questions = MutableLiveData<MutableList<Question>>()
+    private val querySubject = PublishSubject.create<String>()
+    private val repository = StackOverflowRepository()
 
-    fun submitQuery(query: String) {
-
+    init {
+        querySubject.debounce(300, TimeUnit.MILLISECONDS)
+                .filter { it.isNotEmpty() }
+                .distinctUntilChanged()
+                .switchMap { repository.searchQuestions(it) }
+                .subscribe { onDataResponse(it) }
     }
+
+    fun connectionStatus() = repository.connectionAvailable
+    fun submitQuery(query: String) = querySubject.onNext(query)
+
+    private fun onDataResponse(data: QuestionsResponse) {
+        data.items?.let {
+            questions.value = it.toMutableList()
+        }
+    }
+
 }
