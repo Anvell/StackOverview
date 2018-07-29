8b86f6d9673a1858a48516284d00ddd5f0755754
Denis Trotsenko <denis.tnko@gmail.com>
2018-07-29 20:27:05 +0200

    Implemented UI for user's bookmarks

---
+++ app/src/main/java/io/github/anvell/stackoverview/view/CollectionFragment.java
@@ -0,0 +1,68 @@
+package io.github.anvell.stackoverview.view;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.anvell.stackoverview.adapter.BaseAdapter;
+import io.github.anvell.stackoverview.adapter.SearchResultsAdapter;
+import io.github.anvell.stackoverview.databinding.FragmentCollectionBinding;
+import io.github.anvell.stackoverview.enumeration.ActiveScreen;
+import io.github.anvell.stackoverview.model.Question;
+import io.github.anvell.stackoverview.viewmodel.MainViewModel;
+
+public class CollectionFragment extends Fragment implements BaseAdapter.OnInteractionListener<Question> {
+
+    public static final String TAG = "COLLECTION_FRAGMENT";
+
+    private MainViewModel viewModel;
+    private SearchResultsAdapter resultsAdapter;
+    private FragmentCollectionBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentCollectionBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = ViewModelProviders.of(requireActivity()).get(MainViewModel.class);
+        viewModel.activeScreen.setValue(ActiveScreen.COLLECTION);
+        initCollection();
+        initObservers();
+    }
+
+    @Override
+    public void onListInteraction(Question item) {
+        viewModel.requestQuestion(item.questionId);
+    }
+
+    private void initCollection() {
+        LinearLayoutManager linearLayout = new LinearLayoutManager(getContext());
+        resultsAdapter = new SearchResultsAdapter(new ArrayList<>(), this);
+
+        binding.collectionView.setLayoutManager(linearLayout);
+        binding.collectionView.setAdapter(resultsAdapter);
+        binding.collectionView.addItemDecoration(new DividerItemDecoration(getContext(),
+                DividerItemDecoration.VERTICAL));
+    }
+
+    private void initObservers() {
+        viewModel.questionsCollection.observe(this, v -> resultsAdapter.replaceItems(v));
+    }
+}
