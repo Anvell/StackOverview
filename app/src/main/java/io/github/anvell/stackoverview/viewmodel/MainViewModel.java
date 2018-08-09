f958e23d9281a48bf2a82aa89b37614e812c8260
Denis Trotsenko <denis.tnko@gmail.com>
2018-08-09 22:28:40 +0200

    Small fixes

--- app/src/main/java/io/github/anvell/stackoverview/viewmodel/MainViewModel.java
+++ app/src/main/java/io/github/anvell/stackoverview/viewmodel/MainViewModel.java
@@ -38,7 +38,7 @@ public class MainViewModel extends ViewModel {
     private PublishSubject<Query> querySubject = PublishSubject.create();
     private Query lastQuery = new Query("");
     private CompositeDisposable disposables = new CompositeDisposable();
-    public StackOverflowRepository repository = StackOverflowRepository.getInstance();
+    private StackOverflowRepository repository = StackOverflowRepository.getInstance();
 
     @SuppressLint("CheckResult")
     public MainViewModel() {
